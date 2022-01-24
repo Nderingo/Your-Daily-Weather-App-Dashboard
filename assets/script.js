d720652fba8979e343d6bdc85d6071e69dce5505
Emmanuel Lyimo <esal29t@gmail.com>
2022-01-23 20:51:39 -0800

    added index.html file,assets folder and css files

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// Select elements by ID
+ var input = document.getElementById("cityName");
+ var button = document.getElementById("btn");
+ var APIkey = "00089a1cd746d533163603d95646c3b3"
+  var queryURL = "http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}"
+
+ function geoData (){
+
+ }
